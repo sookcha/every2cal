01b3d5ee1562642d17feb6f77fe579f114d76839
Hoseong Son <lsookcha@me.com>
2018-02-09 00:08:35 +0900

    Process from string xml

--- every2cal.py
+++ every2cal.py
@@ -1,4 +1,8 @@
 # -*- coding: utf8 -*-
+import getpass
+
+import everytime
+
 __author__ = "Hoseong Son <me@sookcha.com>"
 
 import argparse
@@ -6,10 +10,24 @@ import argparse
 from convert import Convert
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--xml", type=str, help="Location of timetable xml file", required=True)
+parser.add_argument("--xml", type=str, help="Location of timetable xml file", required=False)
 parser.add_argument("--begin", type=str, help="Semester beginning date", required=True)
 parser.add_argument("--end", type=str, help="Semester ending date", required=True)
 args = parser.parse_args()
 
-c = Convert(args.xml)
+xml = ""
+if (args.xml):
+    xml = args.xml
+
+else:
+    username = input('에브리타임 아이디 :')
+    password = getpass.getpass()
+
+    year = input('가져올 년도 :')
+    semester = input('가져올 학기 :')
+
+    e = everytime.Everytime(username, password)
+    xml = e.get_timetable(year, semester)
+
+c = Convert(xml)
 c.get_calendar(c.get_subjects(), args.begin, args.end)
