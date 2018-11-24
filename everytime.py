919cb4f157d371d12be578e434abc3eb49021863
Hoseong Son <lsookcha@me.com>
2018-11-24 18:54:13 +0900

    Fix broken session
    This fixes #1

--- everytime.py
+++ everytime.py
@@ -14,25 +14,17 @@ class Everytime:
         url = 'https://everytime.kr/user/login'
 
         with requests.Session() as session:
-            index_page_res = session.post(url, data=payload)
-
-            soup = BeautifulSoup(index_page_res.text, 'html.parser')
-            token = soup.find(id="userToken")["value"]
-
             timetable_result = session.post("http://timetable.everytime.kr/ajax/timetable/wizard/getTableList", data={
                 "semester": semester,
-                "year": year,
-                "token": token
+                "year": year
             })
 
             tree = ElementTree.fromstring(timetable_result.text)
             for target in tree.findall('table[@is_primary="1"]'):
                 id = target.get('id')
-                table_xml = session.post("http://timetable.everytime.kr/ajax/timetable/wizard/getOneTable", data={
-                    "id": id,
-                    "token": token
+                table_xml = session.post("https://everytime.kr/find/timetable/table", data={
+                    "id": id
                 })
-                result_xml = table_xml
+                result_xml = table_xml.text
 
-
-        return result_xml.text
+        return result_xml
