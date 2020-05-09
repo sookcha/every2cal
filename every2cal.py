8d49c5182d13c604e993b86cd9c2182f78ab27fb
Haechan Yang <live2skull@gmail.com>
2020-05-09 19:25:12 +0900

    Fix timetable api endpoint url (#3)

--- every2cal.py
+++ every2cal.py
@@ -9,25 +9,30 @@ import argparse
 
 from convert import Convert
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--xml", type=str, help="Location of timetable xml file", required=False)
-parser.add_argument("--begin", type=str, help="Semester beginning date", required=True)
-parser.add_argument("--end", type=str, help="Semester ending date", required=True)
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--xml", type=str, help="Location of timetable xml file", required=False)
+    parser.add_argument("--begin", type=str, help="Semester beginning date", required=True)
+    parser.add_argument("--end", type=str, help="Semester ending date", required=True)
+    args = parser.parse_args()
 
-xml = ""
-if (args.xml):
-    xml = args.xml
+    xml = ""
+    if (args.xml):
+        xml = args.xml
 
-else:
-    username = input('에브리타임 아이디 : ')
-    password = getpass.getpass()
+    else:
+        username = input('에브리타임 아이디 : ')
+        password = getpass.getpass()
 
-    year = input('가져올 년도 : ')
-    semester = input('가져올 학기 : ')
+        year = input('가져올 년도 : ')
+        semester = input('가져올 학기 : ')
 
-    e = everytime.Everytime(username, password)
-    xml = e.get_timetable(year, semester)
+        e = everytime.Everytime(username, password)
+        xml = e.get_timetable(year, semester)
 
-c = Convert(xml)
-c.get_calendar(c.get_subjects(), args.begin, args.end)
+    c = Convert(xml)
+    c.get_calendar(c.get_subjects(), args.begin, args.end)
+
+
+if __name__ == '__main__':
+    main()
